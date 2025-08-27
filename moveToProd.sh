0522f65dd658e0e89e3d92418234ad15a1260f11
Stef-00012 <me@stefdp.com>
2025-08-27 23:38:00 +0200

    update move to prod script for myself

--- moveToProd.sh
+++ moveToProd.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
+
+source ~/.zshrc
 
 agsProdPath="$HOME/dots/homes/stef/programs/widgets/ags/config"
 currentPath=$(pwd)
@@ -15,3 +17,12 @@ cp -r "$currentPath/util" "$agsProdPath"
 cp -r "$currentPath/app.tsx" "$agsProdPath"
 cp -r "$currentPath/style.scss" "$agsProdPath"
 cp -r "$currentPath/tsconfig.json" "$agsProdPath"
+
+cd "$HOME/dots" || exit 1
+git add .
+cd - || exit 1
+
+fr
+
+ags quit -i desktop-shell || echo "ags is not running"
+ags run & disown
